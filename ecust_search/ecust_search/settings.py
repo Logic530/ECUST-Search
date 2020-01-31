7a8981b4b38ccecff2e1effb0cd7411a73ca34da
ubuntu <ubuntu@localhost.localdomain>
2020-01-31 22:58:18 +0800

    setup DATABASES setting

--- ecust_search/ecust_search/settings.py
+++ ecust_search/ecust_search/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'ecust_search.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'HOST': 'localhost',
+        'PASSWORD': 'ECUSTsearch965313Django',
+        'USER': 'django',
     }
 }
 
