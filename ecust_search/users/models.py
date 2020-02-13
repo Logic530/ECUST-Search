59c021592f55bfc2fcc9fc4ddc7148259f4349ff
Sishxo <10180657@mail.ecust.edu.cn>
2020-02-13 19:36:35 +0800

    用django自带的模块写了注册登陆等功能(看懂并照敲了上去)

--- ecust_search/users/models.py
+++ ecust_search/users/models.py
@@ -10,3 +10,24 @@ class User(models.Model):
         db_table="bbs_user"
         verbose_name='用户管理'
         verbose_name_plural=verbose_name
+class LoginUser(AbstractUser):
+    levels = models.PositiveIntegerField(default=0, verbose_name=u'积分')
+    avatar = models.CharField(
+        max_length=200, default='/static/tx/default.jpg', verbose_name=u'头像')
+    privilege = models.CharField(max_length=200, default=0, verbose_name=u'权限')
+    friends = models.ManyToManyField(
+        'self', blank=True, null=True, related_name='friends')
+
+    class Meta:
+        db_table = 'loginuser'
+        verbose_name_plural = u'用户'
+        ordering = ['-date_joined']
+
+    def __unicode__(self):
+        return self.get_username()
+
+    def checkfriend(self, username):
+        if username in self.friends.all():
+            return True
+        else:
+            return False
--- ecust_search/users/views.py
+++ ecust_search/users/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render,redirect
-from django.views import View
-from django.http.request import HttpRequest
-from django.http.response import HttpResponse
+
 from django.contrib.auth.hashers import make_password,check_password
 from django.urls import reverse
 from users.models import User
+
+from django.shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import View
+from forum.form import MessageForm, PostForm, LoginUserForm
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.sites.models import get_current_site
+from django.core.mail import send_mail
+
+
 # Create your views here.
 class RegisterView(View):
     def get(self,request):
@@ -38,3 +47,75 @@ class LoginView(View):
         request.session['id'] = user.id
         request.session['name'] = user.username
         return redirect(reverse('home:index'))
+def userlogin(request, template_name='login.html'):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        next = request.POST['next']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            user.levels += 1  #登录一次积分加 1
+            user.save()
+        return HttpResponseRedirect(next)
+    else:
+        next = request.GET.get('next', None)
+        if next is None:
+            next = reverse_lazy('index')
+        return render_to_response(template_name, {'next': next})
+
+
+#用户注销
+def userlogout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse_lazy('index'))
+
+
+#用户注册
+def userregister(request):
+    if request.method == 'POST':
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+        password_confirm = request.POST.get("password_confirm", "")
+        email = request.POST.get("email", "")
+
+        form = LoginUserForm(request.POST)
+        errors = []
+        #验证表单是否正确
+        if form.is_valid():
+            current_site = get_current_site(request)
+            site_name = current_site.name
+            domain = current_site.domain
+            title = u"欢迎来到 %s ！" % site_name
+            message = u"你好！ %s ,感谢注册 %s ！\n\n" % (username,site_name) + \
+                      u"请牢记以下信息：\n" + \
+                      u"用户名：%s" % username+"\n" + \
+                      u"邮箱：%s" % email+"\n" + \
+                      u"网站：http://%s" % domain+"\n\n"
+            from_email = None
+            try:
+                send_mail(title, message, from_email, [email])
+            except Exception as e:
+                logger.error(
+                    u'[UserControl]用户注册邮件发送失败:[%s]/[%s]' % (username, email))
+                return HttpResponse(u"发送邮件错误!\n注册失败", status=500)
+
+            new_user = form.save()
+            user = authenticate(username=username, password=password)
+            login(request, user)
+        else:
+            #如果表单不正确,保存错误到errors列表中
+            for k, v in form.errors.items():
+                #v.as_text() 详见django.forms.util.ErrorList 中
+                errors.append(v.as_text())
+
+        return render_to_response('user_ok.html', {"errors": errors})
+
+    else:
+        #next = request.GET.get('next',None)
+        #if next is None:
+        #next = reverse_lazy('index')
+        return render_to_response('register.html')
+
+
