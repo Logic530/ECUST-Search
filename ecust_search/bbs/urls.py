2c30f6cebf5ec2a7bc0d59498f3a216a0db3e6eb
Logic_530 <sirui313@gmail.com>
2020-02-13 20:39:44 +0800

    初始化主页视图、板块视图、帖子视图

--- ecust_search/bbs/urls.py
+++ ecust_search/bbs/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('section/<str:section>', views.section, name='section'),
+    path('topic/<int:topic_id>', views.topic, name='topic')
 ]
--- ecust_search/bbs/views.py
+++ ecust_search/bbs/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
+# 主页视图
 def index(request):
-    return HttpResponse("this is the homepage of bbs")
+    return HttpResponse("这是BBS主页视图")
+
+
+# 板块视图
+def section(request, section:str):
+    return HttpResponse("这是板块视图,你正在查看 " + section + " 板块")
+
+
+# 帖子视图
+def topic(request, topic_id:int):
+    return HttpResponse("这是贴子视图," + "你正在查看帖子ID " + str(topic_id))
