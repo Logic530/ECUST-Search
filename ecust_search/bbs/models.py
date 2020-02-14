6e9a7ffbac3f7962630b6c7c8b88d9420fff4fa5
Logic_530 <sirui313@gmail.com>
2020-02-14 14:54:36 +0800

    初步完成模型配置

--- ecust_search/bbs/models.py
+++ ecust_search/bbs/models.py
@@ -1,41 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+# 板块数据模型
+class Section(models.Model):
+    # 板块标题，字符串，最大10
+    title = models.CharField(max_length=10)
+    # 板块描述
+    description = models.CharField(max_length=300)
+    # 板块管理员，多对多关系，不知道配置是否正确
+    admin = models.ManyToManyField(User)
 
 
 # 帖子数据模型
 class Topic(models.Model):
     # 贴子标题，字符串，最大长度30
-    topic_title = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
     # 帖子内容，字符串，最大长度300
-    topic_description = models.CharField(max_length=300)
+    content = models.CharField(max_length=300)
     # 发帖人ID,应该是外键，但是我不懂数据库，先不做
-
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     # 发帖时间
-    topic_publish_datetime = models.DateTimeField()
+    datetime = models.DateTimeField()
     # 帖子所属板块，应该是外键
-
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
     # 帖子被点赞次数
-    topic_liked_count = models.IntegerField()
+    liked_count = models.IntegerField()
 
 
 # 评论数据模型
 class Comment(models.Model):
     # 评论内容
-    comment_content = models.CharField(max_length=300)
-    # 评论发送者ID，外键
-
+    content = models.CharField(max_length=300)
+    # 评论发送者ID，外键，不知配置是否正确
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     # 评论发送日期
-    comment_datetime = models.DateTimeField()
-    # 评论所属帖子,外键
-
+    datetime = models.DateTimeField()
+    # 评论所属帖子,外键，不知配置是否正确
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     # 评论被点赞数
-    comment_liked_count = models.IntegerField()
-    # 评论被踩数
-    comment_disliked_count = models.IntegerField()
-    # 帖子ID,外键
-
-
-# 板块数据模型
-class Section(models.Model):
-    # 板块名称，字符串，最大10
-    section_name = models.CharField(max_length=10)
-    # 还有一些别的什么？我不知道了
+    liked_count = models.IntegerField()
