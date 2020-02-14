0b4d04ef6ed570a6c46da52a84b162c733716859
Logic_530 <sirui313@gmail.com>
2020-02-14 13:55:44 +0800

    添加JS请求帖子列表的API，目前仅占位

--- ecust_search/bbs/urls.py
+++ ecust_search/bbs/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('section/<str:section_name>', views.section, name='section'),
+    path('get-topic', views.get_topic, name='get_topic'),
     path('topic/<int:topic_id>', views.topic, name='topic')
 ]
--- ecust_search/bbs/views.py
+++ ecust_search/bbs/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+import json
 
 
 # 主页视图
@@ -9,6 +10,15 @@ def index(request):
 # 板块视图
 def section(request, section_name: str):
     return HttpResponse("这是板块视图,你正在查看 " + section_name + " 板块")
+
+
+# 请求帖子列表,JS的api，用户不会直接访问
+def get_topic(request):
+    response = {'info': 'Your request information as below',
+                'start': request.GET['start'],
+                'num': request.GET['num']
+                }
+    return HttpResponse(json.dumps(response), content_type='application/json')
 
 
 # 帖子视图
